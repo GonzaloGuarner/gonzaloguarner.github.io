0a29371e3c3eb99d5e707e5e723c836b3a6e1fa6
GonzaloGuarner <86479537+GonzaloGuarner@users.noreply.github.com>
2022-01-23 15:53:16 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,18 @@
 			alignment: 'center'
 		});
 
+	// Video.
+
+		var figure = $(".video").hover(hoverVideo, hideVideo);
+
+		function hoverVideo(e) {
+    		$('video', this).get(0).play();
+		}
+
+		function hideVideo(e) {
+    		$('video', this).get(0).pause();
+		}
+		
 	// Nav.
 
 		// Title Bar.
