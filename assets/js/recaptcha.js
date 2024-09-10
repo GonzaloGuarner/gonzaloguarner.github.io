91315e89352285c5f2743533ed7a2520a00c10bb
GonzaloGuarnerDTT <guarner.gonzalo@digitaltwin.technology>
2024-09-10 18:02:16 +0200

    Revert "captcha update v2->v3 fix"
    This reverts commit fe10f3be63ab7ef1a42d80fff503a4f574174d86.

--- assets/js/recaptcha.js
+++
@@ -1,6 +0,0 @@
-grecaptcha.ready(function() {
-    grecaptcha.execute('6Ldfaz0qAAAAALqlESrI6r6BEySsaG-xp6jd1pQ0', {action: 'submit'}).then(function(token) {
-        document.getElementById('g-recaptcha-response').value = token;
-    });
-});
-
